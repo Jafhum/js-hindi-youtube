6f72b92b16fe0c804376813f3c37ae081dd9d5ac
Jafhum <shaikjafar514@gmail.com>
2026-01-04 17:47:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//primetive datatypes 
+let number = 22; //number datatype
+let string ="jafar"; //string datatype
+let boolean =true; //boolean datatype
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+
+
+
+
+
+
+
+//non-primitive datatypes
