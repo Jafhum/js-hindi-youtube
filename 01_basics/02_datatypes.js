6d52bf6152ae8192e88a34ab049f17c32cc7b043
Jafhum <shaikjafar514@gmail.com>
2026-01-04 18:51:14 +0000

    save process

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -18,6 +18,34 @@ console.log(subject);//undifinded
 let marks = null;
 console.log(typeof marks); //object
 
+//checking the if the datatypes converstions
+
+let sum = 33;
+//changing it's datatype
+let sumUpdate = String(sum); // type of this datatype is number
+console.log(typeof sum);
+console.log(typeof sumUpdate);// type of datatype is String
+
+let read = "javascript";
+let startRead = Boolean(read);
+
+console.log(typeof read);// type of this datatype is string
+console.log(typeof startRead);//type of this datatype is boolean
+
+let add = "33a2"; //here it is string
+let adding = Number(add) //after changing it's datatype it change it's datatype to number 
+
+console.log(add);//before changing it's datatype it is string because we used ""
+console.log(typeof adding);//after changing it's datatype will became to Number
+console.log(adding);//after changing it's datatype the value we stored in it is NaN (not a number)
+
+
+
+
+
+
+
+
 
 
 
