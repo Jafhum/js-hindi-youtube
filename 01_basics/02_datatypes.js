fedee45e0a4c5c273626690ffafa162455b2be96
Jafhum <shaikjafar514@gmail.com>
2026-01-04 17:56:51 +0000

    datat

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,9 +3,22 @@ let number = 22; //number datatype
 let string ="jafar"; //string datatype
 let boolean =true; //boolean datatype
 
+
+// typeof is a function which give us the type of data ex: number string boolean etc
 console.log(typeof number);
 console.log(typeof string);
 console.log(typeof boolean);
+
+//and there are more datatypes like undifined and null
+//this will give you undifined because js will store undifined value auto matically 
+let subject;
+console.log(subject);//undifinded 
+
+//here js will store null value in marks but the null type is object
+let marks = null;
+console.log(typeof marks); //object
+
+
 
 
 
