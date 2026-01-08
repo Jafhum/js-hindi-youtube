79821340032b5a6de9fc6fbc60390eded25ea1e4
Jafhum <shaikjafar514@gmail.com>
2026-01-08 09:46:00 +0000

    save work

--- 01_basics/03_loops&strings.js
+++ 01_basics/03_loops&strings.js
@@ -8,7 +8,7 @@ for (let i=0; i<5; i++){
       console.log("Hello World")
 }
 
-/*tracing 
+/*tracing         output
 i=0 - 0<5 - t - "Hello World" - i++
 i=1 - 1<5 - t - "Hello World" - i++
 i=2 - 2<5 - t - "Hello World" - i++
@@ -25,7 +25,7 @@ while (i<5){
     console.log("Hello Globussoft");
     i++    
 }
-/*
+/*                   output
 i=0 - 0<5 - t - "Hello Globussoft" - i++
 i=1 - 1<5 - t - "Hello Globussoft" - i++
 i=2 - 2<5 - t - "Hello Globussoft" - i++
@@ -56,6 +56,17 @@ this will print atleast once if the conditions is false
 let str ="javascript";
 for(let val of str){
     console.log("val =" , val);
+    /*output
+    val = j
+    val = a
+    val = v
+    val = a
+    val = s
+    val = c
+    val = r
+    val = i
+    val = p
+    val = t*/
     
 }
 //for-in this loop will works in objects
@@ -70,8 +81,71 @@ let student ={
 
 for (let i in student ){
     console.log("Key =", i , student[i]);
+    /*output
+    key= name jafar
+    key = age 23
+    key = cgpa 8.2
+    key = isPass true*/
 }
 
+
+// string can be defined in three ways 
+//1 ' '; single cotes
+
+let str1 = 'jafar';
+console.log(str1 , typeof str1);//jafar string
+
+//2 " "; double cotes
+
+let str2 ="jafar";
+console.log(str2 , typeof str2);//jafar string
+
+//3 ` `; backtik {template litrels} template literals in js is a way to have embedded expressions in strings and ${} this is string interpolation and it is to create string by doing substitution of placeholders 
+
+
+let str3 = "jafar" ;
+console.log(`this is backtik ${str3} and ${typeof str3}`);//this is backtik jafar and string
+
+//we can also add expression in ${Expression}ex
+console.log(`add these number 1 + 2 + 33 and total =${1+2+33}`); //add these number 1 + 2 + 33 and total=36
+
+//Escape Charecter 
+//  backslash n (\n) it make the string in nextling 
+let name = "jafar\njafhum";
+console.log(name); 
+/*output
+jafar
+jafhum 
+*/
+
+//(\t) this will give us space of a tab
+
+let name2 = "jafar\tjafhum";
+console.log(name2);
+/*output
+jafar   jafhum 
+ */
+
+
+// STING METHODS these are built in methods to manipulate a string 
+
+// Note : All strings will not effect the original string they will create a new string that why the string is called Immutable(cann't be  changed)
+
+//toUpperCase() this string menthod is used convert all the charecter in Uppercase
+
+let String1 = "Jafhum";
+console.log(String1.toUpperCase()); // JAFHUM
+
+//toLowerCase() this string menthod is used convert all the charecter in Lower Case
+
+ let String2 = "JafHum";
+console.log(String2.toLowerCase());  //jafhum
+
+
+//trim() this method will remove the start and end spaces   
+
+let string3 = "    Jafar Jafhum     ";
+console.log(string3.trim());//Jafar Jafhum
 
 
 
