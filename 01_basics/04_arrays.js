872ec24df886e95d02ae79e4b0e833b73bb5df09
Jafhum <shaikjafar514@gmail.com>
2026-01-22 07:01:08 +0000

    commit

--- 01_basics/04_arrays.js
+++ 01_basics/04_arrays.js
@@ -1,6 +1,4 @@
 //array is an collection of items
-
-<<<<<<< HEAD
 let arr = [45,56,98,50,78];
 console.log(arr);//[45,56,98,50,78];
 
@@ -106,67 +104,6 @@ count.splice(2,0,102,103);//here addiing items in array from 2 index
 console.log(count);//[ 38, 101, 102, 103, 50,  83,  91]
 
 console.log("hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-//loop on Array
-let heroes = ["Ironman", "Captain America", "Thor", "Hulk"];
-for(let i=1; i<=heroes.length;i++){
-    console.log(heroes[i-1]);
-}
-
-
-//Arrays Methods 
-
-//push() --> this method is used to add items at end of an array and this method will change the orginal array
-
-let Arr = ["angli", "bangli", "chuingli","tangli"];
-
-console.log(Arr); //[angli', 'bangli', 'chuingli', 'tangli']
-
-Arr.push("mangli", "jungli");
-console.log(Arr);//['angli', 'bangli', 'chuingli', 'tangli', 'mangli','jungli']
-
-//pop() --> this method will remove the item at end an Array and this will change the original array
-
-let arr2 = ["bhanush", "thalapathy","boss","Sk"]
-console.log(arr2);//['bhanush', 'thalapathy', 'boss', 'Sk'] 
-
-arr2.pop();
-console.log(arr2);//['bhanush', 'thalapathy', 'boss']
-
-//toString() --> this method will change the whole array into string and this methos will not change the original array this will create a new string
-
-let marks =[36,54,78,69,50];
-console.log(marks);//[ 36, 54, 78, 69, 50 ]
-let marks2 =marks.toString();
-console.log(marks2);//36,54,78,69,50
-
-
-
-//concat() --> this methos is used combine two or more arrays together 
-
-let marvelHeros = ["Antman", "Thor" , "MoonNight"];
-let indianHeros = ["Shaktiman","Panjurli"];
-let dcHeros = ["SuperMan", "Batman","flash"];
-
-let allHeros =marvelHeros.concat(indianHeros,dcHeros);
-
-console.log(allHeros);//[ 'Antman',    'Thor','MoonNight', 'Shaktiman','Panjurli',  'SuperMan','Batman',    'flash']
->>>>>>> dffd2d651e8c2b9b2aaa632e3b153e66bfc0889c
 
 
 
